e64fa50d8feb725d10c58ac3a8f4ef88dd1d6c4b
Anna Szpoton <anna.szpoton@gmail.com>
2022-12-15 11:49:57 +0100

    Add var

--- main.tf
+++ main.tf
@@ -2,8 +2,14 @@ variable "test" {
   type = string
 }
 
+variable "test2" {
+  type = string
+}
+
+
+
 resource "local_file" "test" {
-   content = "${var.test}"
+   content = "${var.test}-${var.test2}"
    filename = "/tmp/foo.bar"
 
 }
